fc34ae649d242c7054cfc5c4138da229328c76ac
ChengluJin <jin.cheng.lu.china@gmail.com>
2019-09-04 03:59:16 +0000

    rpi binaries

--- multi_chain/RO/generation_short_avg/parameters_m_RO.h
+++ multi_chain/RO/generation_short_avg/parameters_m_RO.h
@@ -1,6 +1,6 @@
 #define AES_KEY_LEN 16
 #define AES_PRG_OUT_LEN 32 //in number of bytes
-#define N_SUB_CHAIN 10 //arbitrary positive integer
+#define N_SUB_CHAIN 1000 //arbitrary positive integer
 //#define N_NODE_PER_CHAIN 8192 // 8192 = (2^22 / 512)
 #define N_NODE_PER_CHAIN 64 // 64 = (2^15 / 512)
 
